5ff66e69ed5559d9dc7076ab81c0053e3ba66245
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-11 19:19:47 -0300

    Automatize source formatting for CSS files - Fixes #8

--- tasks/format.js
+++ tasks/format.js
@@ -3,6 +3,29 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
+gulp.task('format-css', function() {
+  return gulp.src('app/styles/**/*.css')
+    .pipe(plugins.autoprefixer([
+      'android >= 4.4',
+      'bb >= 10',
+      'chrome >= 34',
+      'ff >= 30',
+      'ie >= 8',
+      'ie_mob >= 10',
+      'ios >= 7',
+      'opera >= 23',
+      'safari >= 7'
+    ]))
+    .pipe(plugins.csscomb())
+    .pipe(plugins.cssbeautify({
+      indent: '  '
+    }))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(plugins.size({
+      title: 'format-css'
+    }));
+});
+
 gulp.task('format-javascript', function() {
   return gulp.src(['app/scripts/**/*.js'])
   .pipe(plugins.esformatter())
@@ -12,4 +35,4 @@ gulp.task('format-javascript', function() {
   }));
 });
 
-gulp.task('format', ['format-javascript']);
+gulp.task('format', ['format-css', 'format-javascript']);
