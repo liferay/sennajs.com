f7469d6b7175591b333dc180aedb54cd02af6192
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-14 21:02:06 -0300

    Automatize creating SVG/TTF/EOT/WOFF font from several SVG - Fixes #5

--- tasks/build.js
+++ tasks/build.js
@@ -4,6 +4,21 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 
+gulp.task('build-icons', function() {
+  return gulp.src(['app/images/icons/*.svg'])
+    .pipe(plugins.iconfontCss({
+      fontName: 'icons',
+      fontPath: '../images/icons/',
+      path: 'app/styles/icons.css.template',
+      targetPath: '../../styles/icons.css'
+    }))
+    .pipe(plugins.iconfont({
+      fontName: 'icons',
+      normalize: true
+    }))
+    .pipe(gulp.dest('dist/images/icons'));
+});
+
 gulp.task('build-images', function() {
   return gulp.src('app/images/**/*')
     .pipe(plugins.imagemin({
@@ -14,5 +29,5 @@ gulp.task('build-images', function() {
 });
 
 gulp.task('build', ['clean'], function(cb) {
-  runSequence(['build-images'], cb);
+  runSequence(['build-images', 'build-icons'], cb);
 });
