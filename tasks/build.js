06fcb456ddaaf15aa7646b45b8f2ec394dd8d0ba
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-15 16:48:36 -0300

    Automatize SASS parsing - Fixes #6

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
+var util = require('./util');
 
 gulp.task('build-icons', function() {
   return gulp.src('app/images/icons/*.svg')
@@ -22,12 +24,25 @@ gulp.task('build-icons', function() {
 gulp.task('build-images', function() {
   return gulp.src('app/images/**')
     .pipe(plugins.imagemin({
-      progressive: true,
-      interlaced: true
+      interlaced: true,
+      progressive: true
     }))
     .pipe(gulp.dest('dist/images'));
 });
 
+gulp.task('build-css', ['build-images'], function() {
+  return gulp.src('app/styles/**/*.{css,scss}')
+    .pipe(plugins.plumber(util.logError))
+    .pipe(plugins.if('*.scss', plugins.compass({
+      config_file: 'app/styles/.compass',
+      css: 'dist/styles',
+      image: 'dist/images',
+      logging: false,
+      sass: 'app/styles'
+    })))
+    .pipe(gulp.dest('dist/styles'));
+});
+
 gulp.task('build', ['clean'], function(cb) {
-  runSequence(['build-images', 'build-icons'], cb);
+  runSequence(['build-images', 'build-icons', 'build-css'], cb);
 });
