7d17312b868385597539d4186a4b719f6a1ff762
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-15 15:21:24 -0300

    Also check syntax for tasks files - Fixes #7

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('jshint', function() {
-  return gulp.src('app/scripts/**/*.js')
+  return gulp.src(['tasks/*.js', 'app/scripts/**/*.js'])
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
