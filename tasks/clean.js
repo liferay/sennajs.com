e75289f45f10d813361c1919e850642cebfd4758
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-15 15:24:23 -0300

    No need to glob a directory with read false

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,8 +4,5 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('clean', function() {
-  return gulp.src('dist', {
-    read: false
-  })
-  .pipe(plugins.rimraf());
+  return gulp.src('dist').pipe(plugins.rimraf());
 });
