fa939f9e36c9e73940c64b791cb216f6d9e07db5
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-23 00:29:31 -0300

    Moves watch task to its own file

---
+++ tasks/watch.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['build-html']);
+  gulp.watch('src/**/*.soy', ['build-templates']);
+  gulp.watch('src/images/**/*.{gif,jpeg,jpg,png,svg}', ['build-images']);
+  gulp.watch('src/images/icons/*.svg', ['build-icons']);
+  gulp.watch('src/scripts/**/*.js', ['build-javascript']);
+  gulp.watch('src/styles/**/*.css', ['build-css']);
+  gulp.watch('src/styles/**/*.scss', ['build-compass']);
+});
