70c490fb8a07ea49a312ea75c550093a2e2be5c3
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-29 21:30:44 -0300

    Renames jshint task to lint

--- tasks/lint.js
+++ tasks/lint.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
-gulp.task('jshint', function() {
+gulp.task('lint', function() {
   return gulp.src(['tasks/*.js', 'app/scripts/**/*.js'])
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
