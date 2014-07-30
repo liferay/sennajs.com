c06ed794d87f88ca6cf36d9c4e6c484591d1425b
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-30 12:16:19 -0300

    Fixes lint task to read globs from configuration

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,9 +2,10 @@
 
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var config = require('./lib/flavor').generateFlavoredConfig();
 
 gulp.task('lint', function() {
-  return gulp.src(['tasks/*.js', 'app/scripts/**/*.js'])
+  return gulp.src(['tasks/**/*.js', config.globScript])
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
