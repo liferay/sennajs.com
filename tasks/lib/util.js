e59744892604ea410fa009a05ca7ab307258eefa
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-07-29 21:31:13 -0300

    Moves util task to lib/ folder

---
+++ tasks/lib/util.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var gutil = require('gulp-util');
+var lazypipe = require('lazypipe');
+var plugins = require('gulp-load-plugins')();
+var config = require('./flavor').generateFlavoredConfig();
+
+module.exports = {
+  buildCss: buildCssPipeline(),
+  buildHtml: buildHtmlPipeline(),
+  buildHtmlResources: buildHtmlResourcePipeline(),
+  buildJavaScript: buildJavaScriptPipeline(),
+  logError: logError
+};
+
+function buildCssPipeline() {
+  return lazypipe()
+    .pipe(function() {
+      return plugins.if('*.css', plugins.autoprefixer(config.autoprefixer).pipe(plugins.csso()));
+    });
+}
+
+function buildHtmlPipeline() {
+  return lazypipe()
+    .pipe(buildHtmlResourcePipeline())
+    .pipe(function() {
+      return plugins.if('*.html', plugins.minifyHtml());
+    });
+}
+
+function buildHtmlResourcePipeline() {
+  var assets = plugins.useref.assets({
+    searchPath: 'dist'
+  });
+
+  return lazypipe()
+    .pipe(function() {
+      return assets;
+    })
+    .pipe(function() {
+      return assets.restore();
+    })
+    .pipe(buildCssPipeline())
+    .pipe(buildJavaScriptPipeline())
+    .pipe(plugins.useref);
+}
+
+function buildJavaScriptPipeline() {
+  return lazypipe()
+    .pipe(function() {
+      return plugins.if('*.js', plugins.uglify({
+        preserveComments: 'some'
+      }));
+    });
+}
+
+function logError(err) {
+  if (err.fileName) {
+    gutil.log(gutil.colors.red('Error'), err.fileName, lookupErrorLine(err));
+  } else if (err.message) {
+    gutil.log(gutil.colors.red('Error'), err.message);
+  } else {
+    gutil.log(gutil.colors.red('Error'), err);
+  }
+}
+
+function lookupErrorLine(err) {
+  var line = '0';
+  var position = '0';
+
+  if (err.message) {
+    var match = err.message.match(/line #?(\d+):?(\d+)?/i);
+    if (match) {
+      if (match[1]) {
+        line = match[1];
+      }
+      if (match[2]) {
+        position = match[2];
+      }
+    }
+  }
+
+  return line + ':' + position;
+}
