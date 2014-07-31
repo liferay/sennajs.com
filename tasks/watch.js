128cefbce430b702f6c30099bfea9749f02767d0
Zeno Rocha <zeno.rocha@liferay.com>
2014-07-31 14:01:58 -0300

    Connects to a local server on watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 var config = require('./lib/flavor').generateFlavoredConfig();
 
-gulp.task('watch', function() {
+gulp.task('watch', ['build'], function(cb) {
+  plugins.connect.server({
+    root: 'dist'
+  });
+
   gulp.watch(config.globHtml, ['build-html']);
   gulp.watch(config.globIcon, ['build-icons']);
   gulp.watch(config.globImage, ['build-images']);
