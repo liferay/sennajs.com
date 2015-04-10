f233698ebae2da3ec1d644bdff03c6adbc8e903c
Zeno Rocha <hi@zenorocha.com>
2015-04-10 12:06:39 -0700

    :(

--- examples/email/js/menu.js
+++
@@ -1,7 +0,0 @@
-var menuButton = document.querySelector('.nav-menu-button');
-var nav = document.querySelector('#nav');
-
-menuButton.addEventListener('click', function(e) {
-  e.preventDefault();
-  nav.classList.toggle('active');
-});
--- examples/gallery/js/static.js
+++
@@ -1,5 +0,0 @@
-var app = new senna.App();
-
-app.setBasePath('/examples/gallery');
-app.addSurfaces('preview');
-app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
