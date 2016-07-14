453c293faf05362f144bebee1ea39326c4e32467
Dustin Ryerson <dustin.ryerson@liferay.com>
2016-07-14 11:47:34 -0700

    Use senna.js to enable SPA

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,3 +1,14 @@
 (function() {
   'use strict';
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var app = new senna.App();
+    app.setBasePath('/');
+    app.addSurfaces(['body']);
+    app.addRoutes([
+      new senna.Route('/', senna.HtmlScreen),
+      new senna.Route('/about', senna.HtmlScreen),
+      new senna.Route('/docs', senna.HtmlScreen)
+    ]);
+  });
 }());
