92fb4168ffb4639b9d1af554b5c01374f74cd71f
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-03-29 15:22:10 -0700

    Implement electric-plugin-apidocs

--- electric.config.js
+++ electric.config.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var marble = require('marble');
+var path = require('path');
 
 module.exports = {
+	apiConfig: {
+		project: {
+			refs: ['v1.5.3', 'v2.1.3'],
+			repo: 'senna.js',
+			src: 'src/**/*.js',
+			user: 'liferay'
+		}
+	},
 	metalComponents: ['electric-marble-components'],
+	plugins: ['electric-plugin-apidocs'],
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
 	},
